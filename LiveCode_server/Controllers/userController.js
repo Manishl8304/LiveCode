7639bc901672df59336f92aa330c656ed111b84d
Manish Aggarwal <manishaggarwal8304@gmail.com>
2026-03-05 12:42:56 +0530

    debugging

--- LiveCode_server/Controllers/userController.js
+++ LiveCode_server/Controllers/userController.js
@@ -27,6 +27,7 @@ const login = async (req, res) => {
   try {
     await sendOtpEmail(email, otp);
   } catch (err) {
+    console.log(err);
     return res.status(500).json({
       message: "Failed to send OTP",
     });
